1a12581248185fa6c624e818863d0f187d0a842e
Minhyeon Oh <minhyeon.oh@postech.ac.kr>
2024-01-06 19:32:58 +0900

    PEBBLE saves labeled queries

--- rldev/agents/pref/reward_model.py
+++ rldev/agents/pref/reward_model.py
@@ -1,5 +1,6 @@
 
 import numpy as np
+import pickle
 import torch
 import torch as th
 import time
@@ -459,6 +460,13 @@ class RewardModel(Node):
     
     th.save(self._r_optimizer, dir / "_r_optimizer.pt")
     th.save(self._r, dir / "_r.pt")
+    np.save("_segments_first", self.buffer_seg1)
+    np.save("_segments_second", self.buffer_seg2)
+    np.save("_segments_label", self.buffer_label)
+    with open("_segments_cursor.pkl", "wb") as fout:
+      pickle.dump(self.buffer_index, fout)
+    with open("_segments_full.pkl", "wb") as fout:
+      pickle.dump(self.buffer_full, fout)
 
   @overrides
   def load(self, dir: Path):
